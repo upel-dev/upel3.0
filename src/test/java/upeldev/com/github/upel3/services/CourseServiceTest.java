39219e4f27d8aacb5480f3d46d4908646a532593
enkelian <aniano13@gmail.com>
2021-04-07 01:31:33 +0200

    CourseServiceTest added

---
+++ src/test/java/upeldev/com/github/upel3/services/CourseServiceTest.java
@@ -0,0 +1,10 @@
+package upeldev.com.github.upel3.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CourseServiceTest {
+
+
+}
