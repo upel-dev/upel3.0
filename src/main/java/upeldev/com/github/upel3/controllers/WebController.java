f3c14139213a80ab3f27ced25fbc3d389d39c5e3
marcinz99 <marcinzieba1999@gmail.com>
2021-04-10 00:37:50 +0200

    Attached logic to logout button, turned buttons into links, some other minor fixes
    Now the pages are traversable.

--- src/main/java/upeldev/com/github/upel3/controllers/WebController.java
+++ src/main/java/upeldev/com/github/upel3/controllers/WebController.java
@@ -1,13 +1,19 @@
 package upeldev.com.github.upel3.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import upeldev.com.github.upel3.model.User;
 import upeldev.com.github.upel3.services.UserService;
 
 import javax.persistence.Access;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
 @Controller
@@ -17,6 +23,23 @@ public class WebController {
     @Autowired
     public WebController(UserService userService){
         this.userService = userService;
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.POST)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
+    @RequestMapping(value = "/")
+    public String defaultPage(Model model, Principal principal) {
+        User currentUser = userService.findByEmail(principal.getName());
+        model.addAttribute("firstname", currentUser.getFirstName());
+        model.addAttribute("lastname", currentUser.getLastName());
+        return "index";
     }
 
     @RequestMapping(value = "/index")
