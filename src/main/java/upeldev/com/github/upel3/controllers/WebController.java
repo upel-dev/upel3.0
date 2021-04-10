bb309e38263e5883752fd6f16f9729679dff22a2
marcinz99 <marcinzieba1999@gmail.com>
2021-04-10 15:40:46 +0200

    Attached logic to the list of courses on the main page

--- src/main/java/upeldev/com/github/upel3/controllers/WebController.java
+++ src/main/java/upeldev/com/github/upel3/controllers/WebController.java
@@ -8,21 +8,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import upeldev.com.github.upel3.model.Course;
 import upeldev.com.github.upel3.model.User;
+import upeldev.com.github.upel3.services.CourseService;
 import upeldev.com.github.upel3.services.UserService;
 
 import javax.persistence.Access;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class WebController {
     private final UserService userService;
+    private final CourseService courseService;
 
     @Autowired
-    public WebController(UserService userService){
+    public WebController(UserService userService, CourseService courseService){
         this.userService = userService;
+        this.courseService = courseService;
     }
 
     @RequestMapping(value="/logout", method = RequestMethod.POST)
@@ -37,40 +42,58 @@ public class WebController {
     @RequestMapping(value = "/")
     public String defaultPage(Model model, Principal principal) {
         User currentUser = userService.findByEmail(principal.getName());
-        model.addAttribute("firstname", currentUser.getFirstName());
-        model.addAttribute("lastname", currentUser.getLastName());
+        List<Course> courses = currentUser.getCoursesLectured();
+        courses.addAll(currentUser.getCoursesEnrolledIn());
+        model.addAttribute("courses", courses);
+        model.addAttribute("user", currentUser);
         return "index";
     }
 
     @RequestMapping(value = "/index")
     public String index(Model model, Principal principal) {
         User currentUser = userService.findByEmail(principal.getName());
-        model.addAttribute("firstname", currentUser.getFirstName());
-        model.addAttribute("lastname", currentUser.getLastName());
+        List<Course> courses = currentUser.getCoursesLectured();
+        courses.addAll(currentUser.getCoursesEnrolledIn());
+        model.addAttribute("courses", courses);
+        model.addAttribute("user", currentUser);
         return "index";
     }
 
     @RequestMapping(value = "/course")
     public String course(Model model, Principal principal) {
         User currentUser = userService.findByEmail(principal.getName());
-        model.addAttribute("firstname", currentUser.getFirstName());
-        model.addAttribute("lastname", currentUser.getLastName());
+        model.addAttribute("user", currentUser);
+        return "course";
+    }
+
+    @RequestMapping(value = "/course", method = RequestMethod.GET)
+    public String courseSpecific(Model model, Principal principal, HttpServletRequest request) {
+        User currentUser = userService.findByEmail(principal.getName());
+        model.addAttribute("user", currentUser);
+        try {
+            Long id = Long.parseLong(request.getParameter("id").toString());
+            Course course = courseService.findCourseById(id);
+            model.addAttribute("course", course);
+        }
+        catch(NumberFormatException nfe) {
+            String errorMsg = "Zapytanie GET /course?id=<course_id> otrzymało niewłaściwy typ danych. Spodziewany typ: long integer.";
+            model.addAttribute("errorMsg", errorMsg);
+            return "error";
+        }
         return "course";
     }
 
     @RequestMapping(value = "/new_course")
     public String new_course(Model model, Principal principal) {
         User currentUser = userService.findByEmail(principal.getName());
-        model.addAttribute("firstname", currentUser.getFirstName());
-        model.addAttribute("lastname", currentUser.getLastName());
+        model.addAttribute("user", currentUser);
         return "new_course";
     }
 
     @RequestMapping(value = "/profile")
     public String profile(Model model, Principal principal) {
         User currentUser = userService.findByEmail(principal.getName());
-        model.addAttribute("firstname", currentUser.getFirstName());
-        model.addAttribute("lastname", currentUser.getLastName());
+        model.addAttribute("user", currentUser);
         return "profile";
     }
 }
