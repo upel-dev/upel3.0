cfedf4d75f4296a4894a17b22d22c61cae9ee88e
enkelian <aniano13@gmail.com>
2021-04-11 14:10:22 +0200

    activity template stub

--- src/main/java/upeldev/com/github/upel3/controllers/ActivityController.java
+++ src/main/java/upeldev/com/github/upel3/controllers/ActivityController.java
@@ -4,6 +4,7 @@ package upeldev.com.github.upel3.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import upeldev.com.github.upel3.auth.Upel3UserDetails;
 import upeldev.com.github.upel3.model.Activity;
@@ -11,6 +12,8 @@ import upeldev.com.github.upel3.model.User;
 import upeldev.com.github.upel3.services.ActivityService;
 import upeldev.com.github.upel3.services.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -18,7 +21,6 @@ import java.util.List;
 public class ActivityController {
     private final ActivityService activityService;
     private final UserService userService;
-
 
     @Autowired
     public ActivityController(ActivityService activityService, UserService userService) {
@@ -42,5 +44,23 @@ public class ActivityController {
         }
 
 
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String activity(Model model, Principal principal, HttpServletRequest request){
+        User currentUser = userService.findByEmail(principal.getName());
+        model.addAttribute("user", currentUser);
+        try {
+            Long id = Long.parseLong(request.getParameter("id").toString());
+            Activity activity = activityService.findActivityById(id);
+            model.addAttribute("activity", activity);
+            model.addAttribute("course", activity.getCourse());
+        }
+        catch(NumberFormatException nfe) {
+            String errorMsg = "Zapytanie GET /activity?id=<activity_id> otrzymało niewłaściwy typ danych. Spodziewany typ: long integer.";
+            model.addAttribute("errorMsg", errorMsg);
+            return "error";
+        }
+        return "activity_student";
     }
 }
