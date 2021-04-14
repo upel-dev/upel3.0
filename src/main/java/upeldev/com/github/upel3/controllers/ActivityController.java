c46bc6af07026e53d8ad31e14fbe59d3bb3cbcae
enkelian <aniano13@gmail.com>
2021-04-14 15:06:03 +0200

    fixed grades bug

--- src/main/java/upeldev/com/github/upel3/controllers/ActivityController.java
+++ src/main/java/upeldev/com/github/upel3/controllers/ActivityController.java
@@ -7,27 +7,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import upeldev.com.github.upel3.auth.Upel3UserDetails;
-import upeldev.com.github.upel3.model.Activity;
-import upeldev.com.github.upel3.model.Course;
-import upeldev.com.github.upel3.model.Role;
-import upeldev.com.github.upel3.model.User;
+import upeldev.com.github.upel3.model.*;
 import upeldev.com.github.upel3.services.ActivityService;
+import upeldev.com.github.upel3.services.GradeService;
 import upeldev.com.github.upel3.services.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path="/activity")
 public class ActivityController {
     private final ActivityService activityService;
     private final UserService userService;
+    private final GradeService gradeService;
 
     @Autowired
-    public ActivityController(ActivityService activityService, UserService userService) {
+    public ActivityController(ActivityService activityService, UserService userService, GradeService gradeService) {
         this.activityService = activityService;
         this.userService = userService;
+        this.gradeService = gradeService;
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
@@ -66,11 +67,28 @@ public class ActivityController {
             }
 
             model.addAttribute("activity", activity);
+            List<Grade> grades;
+            if(currentUser.getRoles().contains(Role.STUDENT)){
+                grades = gradeService.findGradeByUser(currentUser);
+                grades = grades
+                        .stream()
+                        .filter(grade -> grade.getActivity().equals(activity)
+                                && grade.getUser().equals(currentUser))
+                        .collect(Collectors.toList());
+            }
+            else{
+                grades = gradeService.findAll()
+                        .stream()
+                        .filter(grade -> grade.getActivity().equals(activity))
+                        .collect(Collectors.toList());
+            }
+
+            model.addAttribute("grades", grades);
 
             Course course = activity.getCourse();
             model.addAttribute("course", course);
 
-            model.addAttribute("grades", activity.getGrade());
+//            model.addAttribute("grades", activity.getGrade());
 
             if(currentUser.getCoursesEnrolledIn().contains(course)) return "activity_student";
             if(currentUser.getCoursesLectured().contains(course)) return "activity_lecturer";
