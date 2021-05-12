dadf77f10879790d7a4736510cab5aca0ed46a79
xenoteo <fedorovxa@gmail.com>
2021-05-12 18:38:14 +0200

    StudentGroupService written

--- src/main/java/upeldev/com/github/upel3/repositories/StudentGroupRepository.java
+++ src/main/java/upeldev/com/github/upel3/repositories/StudentGroupRepository.java
@@ -1,7 +1,20 @@
 package upeldev.com.github.upel3.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import upeldev.com.github.upel3.model.Course;
 import upeldev.com.github.upel3.model.StudentGroup;
+import upeldev.com.github.upel3.model.User;
+
+import java.util.List;
 
 public interface StudentGroupRepository extends CrudRepository<StudentGroup, Integer> {
+
+    StudentGroup findById(Long id);
+
+    List<StudentGroup> findByCourse(Course course);
+
+    List<StudentGroup> findByName(String name);
+
+    List<StudentGroup> findByStudentsContains(User user);
+
 }
--- src/main/java/upeldev/com/github/upel3/services/StudentGroupService.java
+++ src/main/java/upeldev/com/github/upel3/services/StudentGroupService.java
@@ -1,4 +1,76 @@
 package upeldev.com.github.upel3.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import upeldev.com.github.upel3.model.Course;
+import upeldev.com.github.upel3.model.StudentGroup;
+import upeldev.com.github.upel3.model.User;
+import upeldev.com.github.upel3.repositories.StudentGroupRepository;
+import upeldev.com.github.upel3.repositories.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
 public class StudentGroupService {
+    private final StudentGroupRepository studentGroupRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public StudentGroupService(StudentGroupRepository studentGroupRepository, UserRepository userRepository) {
+        this.studentGroupRepository = studentGroupRepository;
+        this.userRepository = userRepository;
+    }
+
+    public StudentGroup save(StudentGroup group){
+        return studentGroupRepository.save(group);
+    }
+
+    public List<StudentGroup> findAll(){
+        return StreamSupport.stream(studentGroupRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+    StudentGroup findById(Long id){
+        return studentGroupRepository.findById(id);
+    }
+
+    List<StudentGroup> findByCourse(Course course){
+        return studentGroupRepository.findByCourse(course);
+    }
+
+    List<StudentGroup> findByName(String name){
+        return studentGroupRepository.findByName(name);
+    }
+
+    List<StudentGroup> findByStudent(User student){
+        return studentGroupRepository.findByStudentsContains(student);
+    }
+
+    public void changeName(StudentGroup group, String newName){
+        if (newName == null || newName.isEmpty()){
+            throw new IllegalArgumentException("New group name cannot be empty");
+        }
+
+        group.setName(newName);
+        studentGroupRepository.save(group);
+    }
+
+    public void addStudentToGroup(StudentGroup group, User student){
+        group.getStudents().add(student);
+        studentGroupRepository.save(group);
+
+        student.getGroups().add(group);
+        userRepository.save(student);
+    }
+
+    public void removeStudentFromGroup(StudentGroup group, User student){
+        group.getStudents().remove(student);
+        studentGroupRepository.save(group);
+
+        student.getGroups().remove(group);
+        userRepository.save(student);
+    }
+
 }
