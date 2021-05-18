3ce995ec65bac062257aabd81ae25034dd778bb3
enkelian <aniano13@gmail.com>
2021-05-18 21:49:16 +0200

    prettier data

--- src/main/java/upeldev/com/github/upel3/DataLoader.java
+++ src/main/java/upeldev/com/github/upel3/DataLoader.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Component;
 import upeldev.com.github.upel3.model.*;
 import upeldev.com.github.upel3.services.*;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class DataLoader {
@@ -42,14 +41,22 @@ public class DataLoader {
     }
 
     public void populateCourses(){
-        Course firstCourse = courseService.createCourse("First course", "first description", userService.findByEmail("kate@gmail.com"));
+        Course firstCourse = courseService.createCourse("Systemy operacyjne", "Celem wykładu jest zapoznanie z budową i zasadą działania systemu operacyjnego, jego elementami składowymi oraz z problemami jakie należy rozwiązać, budując nowy system.", userService.findByEmail("kate@gmail.com"));
+
         courseService.addStudentToCourse(firstCourse.getId(), "benjamin@gmail.com");
         courseService.addStudentToCourse(firstCourse.getId(), "bmw@gmail.com");
 
-        Course secondCourse = courseService.createCourse("Second course", "second description", userService.findByEmail("kate@gmail.com"));
+        firstCourse.setPassValue(100.0);
+        courseService.save(firstCourse);
+
+        Course secondCourse = courseService.createCourse("Inżynieria oprogramowania", "Cykl życia oprogramowania. Proces produkcji oprogramowania. Inżynieria systemów komputerowych. Zarządzanie przedsięwzięciami w inżynierii oprogramowania. Wymagania wobec oprogramowania i proces inżynierii wymagań. ", userService.findByEmail("kate@gmail.com"));
         courseService.addStudentToCourse(secondCourse.getId(), "benjamin@gmail.com");
 
+        secondCourse.setPassValue(100.0);
+
         userService.hideCourse(userService.findByEmail("benjamin@gmail.com"), secondCourse);
+
+        courseService.save(secondCourse);
     }
 
     public void populateGroups(){
@@ -64,11 +71,11 @@ public class DataLoader {
     }
 
     public void populateUsers() {
-        User john = new User("John", "Doe", "john@gmail.com", "1234");
+        User john = new User("Jan", "Kowalski", "john@gmail.com", "1234");
         john.getRoles().add(Role.ADMIN);
         userService.registerNewUser(john);
 
-        User kate = new User("Kate", "Smith", "kate@gmail.com", "1234");
+        User kate = new User("Katarzyna", "Kowal", "kate@gmail.com", "1234");
         kate.getRoles().add(Role.LECTURER);
         userService.registerNewUser(kate);
 
@@ -77,42 +84,63 @@ public class DataLoader {
         benjamin.setIndexNumber("123456");
         userService.registerNewUser(benjamin);
 
-        User bmw = new User("Benjamin", "Bmw", "bmw@gmail.com", "1234");
+        User bmw = new User("Zygfryd", "Kaczmarczyk", "bmw@gmail.com", "1234");
         bmw.getRoles().add(Role.STUDENT);
         bmw.setIndexNumber("123457");
         userService.registerNewUser(bmw);
 
     }
     private void populateActivity(){
+
         List<Course> allCourses = courseService.findAll();
-        for(int i = 0; i < 5; i++){
-            Activity activityToAppend = new Activity(allCourses.get(i%allCourses.size()), 100, "Kolokwium " + i);
-            activityToAppend.setDescription("Opis tej aktywności pewnie jest bardzo znaczący xd");
-            activityService.save(activityToAppend);
+
+        List<String> names = List.of("Zestaw", "Kolokwium");
+
+        List<String> descriptions = List.of(
+                "Zaprojektuj i przygotuj zestaw funkcji (bibliotekę) do zarządzania tablicą bloków",
+                "Celem zadania jest napisanie programu porównującego wydajność systemowych i bibliotecznych funkcji wejścia/wyjścia. Program operował będzie na przechowywanej w pliku tablicy rekordów.",
+                "Napisz program monitor do robienia kopii zapasowych plików, zawartych w pliku lista podawanym jako argument programu.",
+                "Napisz dwa programy: sender program wysyłający sygnały SIGUSR1 i catcher - program zliczający ilość odebranych sygnałów."
+        );
+
+        for(int c = 0; c < allCourses.size(); c++) {
+            for (int i = 0; i < descriptions.size(); i++) {
+                Activity newActivity = new Activity(allCourses.get(c), 10, names.get(c) + " "  + (i + 1));
+                newActivity.setDescription(descriptions.get(i));
+                activityService.save(newActivity);
+            }
         }
 
     }
     private void populateGrade() {
+
         List<User> users = userService.findAll();
         List<Activity> activities = activityService.findAll();
 
-        for (int i = 0; i < 25; i++) {
-            User user = users.get(i % users.size());
-            Activity activity = activities.get(i % activities.size());
-            Course course = activity.getCourse();
-            if(!activity.getCourse().getLecturers().contains(user) && activity.getCourse().getEnrolledStudents().contains(user) && gradeService.findGradeByCourseAndUserAndActivity(course, user, activity).size()==0) {
-                StudentGrade grade = new StudentGrade(user, activity);
-                grade.setDescription("Opis " + i);
+        for (int i = 0; i < activities.size(); i++) {
+            Activity activity = activities.get(i);
+
+            Set<User> usersInCourse = activity.getCourse().getEnrolledStudents();
+            Iterator<User> userIterator = usersInCourse.iterator();
+
+            for(int j = 0; j < usersInCourse.size(); j++){
+
+                User currentUser = userIterator.next();
+
+                StudentGrade grade = new StudentGrade(currentUser, activity);
+                grade.setDescription("Opis " + (j + 1));
                 gradeService.save(grade);
+
             }
         }
+
     }
 
     private void populateSubActivity() {
         List<Activity> activities = activityService.findAll();
 
         for (int i = 0; i < 25; i++) {
-            SubActivity subActivity = new SubActivity(activities.get(i%activities.size()), 10, "Zadanie " + i);
+            SubActivity subActivity = new SubActivity(activities.get(i%activities.size()), 10, "Zadanie " + (i + 1));
             subActivityService.save(subActivity);
         }
     }
@@ -120,7 +148,7 @@ public class DataLoader {
         List<SubActivity> subActivities = subActivityService.findAll();
         for(SubActivity subActivity : subActivities){
             for(Grade grade : subActivity.getActivity().getGrades()){
-                SubGrade subGrade = new SubGrade(subActivity, grade, (int)(Math.random()*40));
+                SubGrade subGrade = new SubGrade(subActivity, grade, (int)(Math.random()*subActivity.getMaxValue()));
                 subGradeService.save(subGrade);
             }
         }
